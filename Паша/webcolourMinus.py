e13f97dcfd7b795512141a0bc390dc806697e4dd
HURROLED <76488949+HURROLED@users.noreply.github.com>
2021-10-12 20:33:33 +0500

    Add files via upload
    Красит пиксель!!!!!

---
+++ Паша/webcolourMinus.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+ 
+while(True):
+    ret, frame = cap.read() #берем картинку с вебки
+    frame = cv2.flip(frame, 1) # ПЕРЕВЕРНИТЕ КАМЕРУ ОБРАТНО
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    hsv_img=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)# конвертируем изображение в hsv
+    frame[0,10]=[255, 255, 255]
+    color_low= (0, 30, 60)# нижний диапозон цвета
+    color_hight= (20, 150, 255)#верхний диапозон цвета
+    ol=cv2.inRange(hsv_img,color_low,color_hight)#наложение цветовой маски на изображение, ol присваивает результат
+    cv2.imshow("wwww",ol)
+    cv2.imshow("1",frame)
+
+    
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
