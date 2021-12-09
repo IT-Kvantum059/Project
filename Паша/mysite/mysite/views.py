c1e697cf7a09f15b059ecf1fd337e81e007e042e
white-angle-star <mts04@inbox.ru>
2021-12-09 19:30:33 +0500

    move

--- Паша/mysite/mysite/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-def show_phones(request):
-    template = loader.get_template('snakes.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
