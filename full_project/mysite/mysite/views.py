cf3348c4229fbaad4d3448b6df000050c44469ee
white-andle-star <mts04@inbox.ru>
2021-12-14 21:29:30 +0500

    create cmd

--- full_project/mysite/mysite/views.py
+++ full_project/mysite/mysite/views.py
@@ -1,8 +1,31 @@
-from django.http import HttpResponse
+import os
+import json
+from django.views.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
-def show_phones(request):
+def index(request):
     template = loader.get_template('snakes.html')
     context = {}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
+
+def input_commands(request):
+    cmd = request.GET.get('cmd', None)
+    res = {}
+    if cmd:
+        with open('cmd.json', 'w', encoding='utf-8') as f:
+            json.dump({'cmd': cmd}, f)
+            res = {'stat': 'cmd_set'}
+    else:
+        with open('cmd.json', 'w', encoding='utf-8') as f:
+            json.dump({'cmd': ''}, f)
+            res = {'stat': 'cmd_unset'}
+    return JsonResponse(res)
+
+def output_commands(request):
+    cmd = {
+        'cmd': os.environ.pop('CMD') # read line with cmd
+    }
+
+    return JsonResponse(cmd)
